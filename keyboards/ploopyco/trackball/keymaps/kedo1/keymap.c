bd914141c5d766906bb3462eb79a223573976dad
Kenny Do <kedo@render.com>
2025-05-16 22:00:26 -0700

    use combo for mission control

--- keyboards/ploopyco/trackball/keymaps/kedo1/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/kedo1/keymap.c
@@ -17,74 +17,14 @@
  */
 #include QMK_KEYBOARD_H
 
-// Tap Dance declarations
-enum {
-    TD_MOUSE_BTN5,
-};
-
-typedef enum {
-    TD_UNKNOWN,
-    TD_SINGLE_TAP,
-    TD_SINGLE_HOLD,
-} td_state_t;
-
-// Create a global instance of the tapdance state type
-static td_state_t td_state;
-
-// Function to determine the current tapdance state
-td_state_t cur_dance(tap_dance_state_t *state);
-
-void mouse_btn5_td_finished(tap_dance_state_t *state, void *user_data) {
-    td_state = cur_dance(state);
-    switch (td_state) {
-        case TD_SINGLE_TAP:
-            tap_code(KC_BTN5);
-            break;
-        case TD_SINGLE_HOLD:
-            layer_on(1);
-            break;
-        default:
-            break;
-    }
-}
-
-void mouse_btn5_td_reset(tap_dance_state_t *state, void *user_data) {
-    switch (td_state) {
-        case TD_SINGLE_HOLD:
-            layer_off(1);
-            break;
-        default:
-            break;
-    }
-}
-
-// Tap Dance definitions
-tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Caps Lock
-    [TD_MOUSE_BTN5] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, mouse_btn5_td_finished, mouse_btn5_td_reset),
+const uint16_t PROGMEM combo1[] = {KC_BTN1, KC_BTN5, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(combo1, LCTL(KC_UP)),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( /* Base */
         KC_BTN1, KC_BTN3, KC_BTN2,
-          KC_BTN4, TD(TD_MOUSE_BTN5)
-    ),
-    [1] = LAYOUT(
-        LCTL(KC_UP), KC_TRANSPARENT, KC_TRANSPARENT,
-          KC_TRANSPARENT, KC_TRANSPARENT
+          KC_BTN4, KC_BTN5
     ),
 };
-
-td_state_t cur_dance(tap_dance_state_t *state) {
-    if (state->count == 1) {
-        // Interrupted means some other button was pressed in the tapping term
-        if (state->interrupted || !state->pressed) {
-            return TD_SINGLE_TAP;
-        } else {
-            // Key has not been interrupted, but the key is still held. Means you want to send a 'HOLD'.
-            return TD_SINGLE_HOLD;
-        }
-    }
-
-    return TD_UNKNOWN;
-}
